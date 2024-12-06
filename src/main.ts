c79366fc6f4cd2ed90239bd49dfa475071fcf76c
Parker <parkerharmon194@gmail.com>
2024-12-05 21:12:57 -0500

    Added and initialized firebase

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ import { fa } from "vuetify/iconsets/fa";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import router from "./router";
-
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getFirestore, Firestore } from "firebase/firestore";
 const vuetify = createVuetify({
   icons: {
     defaultSet: "mdi",
@@ -21,5 +22,18 @@ const vuetify = createVuetify({
   components,
   directives,
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx3O7zO97ORRGwClNoGFY-eENqbBoRMRQ",
+  authDomain: "webdev-project5.firebaseapp.com",
+  projectId: "webdev-project5",
+  storageBucket: "webdev-project5.firebasestorage.app",
+  messagingSenderId: "176860175743",
+  appId: "1:176860175743:web:5b8bb15640749e83984e4a",
+  measurementId: "G-F7X19QV78M"
+};
+
+const myapp: FirebaseApp = initializeApp(firebaseConfig)
+const db: Firestore = getFirestore(myapp)
 const pinia = createPinia();
 createApp(App).use(vuetify).use(pinia).use(router).mount("#app");
